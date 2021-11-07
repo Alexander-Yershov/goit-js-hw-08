0fa61b51f526976873991955e73e5d7716442944
Alexander-Yershov <alex04398956@gmail.com>
2021-11-07 14:46:39 +0100

    завершает модуль 8

---
+++ src/js/01-gallery.js
@@ -0,0 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({preview,original,description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join('');    
+};
+const modalLightbox = new SimpleLightbox('.gallery a', {
+  captionData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
